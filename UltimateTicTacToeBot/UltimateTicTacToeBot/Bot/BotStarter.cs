ea48070b72b087f8a59396055c6767a895b10e86
programmeergod <tom12697@gmail.com>
2019-03-26 19:07:27 +0100

    Removed obsolete code

--- UltimateTicTacToeBot/UltimateTicTacToeBot/Bot/BotStarter.cs
+++ UltimateTicTacToeBot/UltimateTicTacToeBot/Bot/BotStarter.cs
@@ -1,36 +1,425 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace UltimateTicTacToeBot.Bot
 {
     class BotStarter
     {
-        private Random rand = new Random();
-
-		/// <summary>
-		/// Returns the next move to make. Edit this method to make your bot smarter.
-		/// Currently does only random moves.
-		/// </summary>
-		/// <param name="state"></param>
-		/// <returns>The column where the turn was made</returns>
-		public Move GetMove(BotState state)
-        {
-            var moves = state.Field.GetAvailableMoves();
-
-            if (moves.Count > 0)
-            {
-                // get random move from available moves
-                return moves.ElementAt(rand.Next(moves.Count)); 
-            }
-
-            // pass
-            return null;
-        }
+        private readonly string empty = ".";
 
         static void Main(string[] args)
         {
             BotParser parser = new BotParser(new BotStarter());
             parser.Run();
+        }
+
+        public Move MiniMax(string[,] macroboard, string[,] board, string p1, string p2, BotState currentState)
+        {
+            var moves = currentState.Field.GetAvailableMoves();
+            var scores = new List<int>();
+
+            var allMoves = new List<Move>();
+
+            foreach (var m in moves)
+            {
+                board[m.X, m.Y] = p1;
+                scores.Add(CalculateScore(macroboard, board, m, GetTopLeft(m), p1, p2, true));
+                allMoves.Add(m);
+
+                board[m.X, m.Y] = p2;
+                int score = CalculateScore(macroboard, board, m, GetTopLeft(m), p1, p2, false);
+                if (score > 0)
+                {
+                    scores.Add(score);
+                    allMoves.Add(m);
+                }
+
+                board[m.X, m.Y] = empty;
+            }
+
+            return BestMove(scores, allMoves);
+        }
+
+        public bool CheckWinGame(string[,] board, string player)
+        {
+            return (
+                board[0, 0].Equals(player) && board[1, 0].Equals(player) && board[2, 0].Equals(player) ||
+                board[0, 1].Equals(player) && board[1, 1].Equals(player) && board[2, 1].Equals(player) ||
+                board[0, 2].Equals(player) && board[1, 2].Equals(player) && board[2, 2].Equals(player) ||
+
+                board[0, 0].Equals(player) && board[0, 1].Equals(player) && board[0, 2].Equals(player) ||
+                board[1, 0].Equals(player) && board[1, 1].Equals(player) && board[1, 2].Equals(player) ||
+                board[2, 0].Equals(player) && board[2, 1].Equals(player) && board[2, 2].Equals(player) ||
+
+                board[0, 0].Equals(player) && board[1, 1].Equals(player) && board[2, 2].Equals(player) ||
+                board[0, 2].Equals(player) && board[1, 1].Equals(player) && board[2, 0].Equals(player));
+        }
+
+        public int CalculateScore(string[,] macroboard, string[,] board, Move move, Move topLeft, string p1, string p2, bool isp1)
+        {
+            if (CheckWinGame(macroboard, p1))
+            {
+                return 1000;
+            }
+            else if (CheckWinGame(macroboard, p2))
+            {
+                return -1000;
+            }
+            else if (CheckWinTile(topLeft, board, p1))
+            {
+                return 100;
+            }
+            else if (CheckWinTile(topLeft, board, p2))
+            {
+                return -100;
+            }
+            else if (isp1)
+            {
+                return CheckGameWonOrFull(BestInRow(board, move, topLeft, p1), macroboard, board, move, topLeft, p1, p2);
+            }
+            else
+            {
+                return -BestInRow(board, move, topLeft, p2);
+            }
+        }
+        public Move GetTopLeft(Move move)
+        {
+            var x = 0;
+            var y = 0;
+
+            if (move.X >= 0 && move.X <= 2)
+            {
+                x = 0;
+            }
+            else if (move.X >= 3 && move.X <= 5)
+            {
+                x = 3;
+            }
+            else
+            {
+                x = 6;
+            }
+
+            if (move.Y >= 0 && move.Y <= 2)
+            {
+                y = 0;
+            }
+            else if (move.Y >= 3 && move.Y <= 5)
+            {
+                y = 3;
+            }
+            else
+            {
+                y = 6;
+            }
+
+            return new Move(x, y);
+        }
+
+        public bool CheckWinTile(Move topLeft, string[,] board, string player)
+        {
+            return (board[topLeft.X, topLeft.Y].Equals(player) && board[topLeft.X + 1, topLeft.Y].Equals(player) && board[topLeft.X + 2, topLeft.Y].Equals(player) ||
+                board[topLeft.X, topLeft.Y + 1].Equals(player) && board[topLeft.X + 1, topLeft.Y + 1].Equals(player) && board[topLeft.X + 2, topLeft.Y + 1].Equals(player) ||
+                board[topLeft.X, topLeft.Y + 2].Equals(player) && board[topLeft.X + 1, topLeft.Y + 2].Equals(player) && board[topLeft.X + 2, topLeft.Y + 2].Equals(player) ||
+
+                board[topLeft.X, topLeft.Y].Equals(player) && board[topLeft.X, topLeft.Y + 1].Equals(player) && board[topLeft.X, topLeft.Y + 2].Equals(player) ||
+                board[topLeft.X + 1, topLeft.Y].Equals(player) && board[topLeft.X + 1, topLeft.Y + 1].Equals(player) && board[topLeft.X + 1, topLeft.Y + 2].Equals(player) ||
+                board[topLeft.X + 2, topLeft.Y].Equals(player) && board[topLeft.X + 2, topLeft.Y + 1].Equals(player) && board[topLeft.X + 2, topLeft.Y + 2].Equals(player) ||
+
+                board[topLeft.X, topLeft.Y].Equals(player) && board[topLeft.X + 1, topLeft.Y + 1].Equals(player) && board[topLeft.X + 2, topLeft.Y + 2].Equals(player) ||
+                board[topLeft.X + 2, topLeft.Y].Equals(player) && board[topLeft.X + 1, topLeft.Y + 1].Equals(player) && board[topLeft.X, topLeft.Y + 2].Equals(player));
+        }
+
+        public Move BestMove(List<int> scores, List<Move> moves)
+        {
+            var p1bestmove = scores.Max();
+            var p2bestmove = scores.Min();
+
+            if (p1bestmove >= (p2bestmove * -1))
+            {
+                return moves[scores.IndexOf(p1bestmove)];
+            }
+            else
+            {
+                return moves[scores.IndexOf(p2bestmove)];
+            }
+        }
+        public int BestInRow(string[,] board, Move move, Move topLeft, string player)
+        {
+            var score = 0;
+
+            var originMove = new Move(move.X - topLeft.X, move.Y - topLeft.Y);
+
+            if (originMove.X == 0)
+            {
+                if (originMove.Y == 0)
+                {
+                    score += CalculateRowValueOne(board, topLeft, player);
+                    score += CalculateColumnValueOne(board, topLeft, player);
+                    score += CalculateDiagonalValueOne(board, topLeft, player);
+                }
+                else if (originMove.Y == 1)
+                {
+                    score += CalculateRowValueTwo(board, topLeft, player);
+                    score += CalculateColumnValueOne(board, topLeft, player);
+                }
+                else
+                {
+                    score += CalculateColumnValueOne(board, topLeft, player);
+                    score += CalculateColumnValueThree(board, topLeft, player);
+                    score += CalculateDiagonalValueTwo(board, topLeft, player);
+                }
+            }
+            else if (originMove.X == 1)
+            {
+                if (originMove.Y == 0)
+                {
+                    score += CalculateRowValueOne(board, topLeft, player);
+                    score += CalculateColumnValueTwo(board, topLeft, player);
+                }
+                else if (originMove.Y == 1)
+                {
+                    score += CalculateColumnValueTwo(board, topLeft, player);
+                    score += CalculateRowValueTwo(board, topLeft, player);
+                    score += CalculateDiagonalValueOne(board, topLeft, player);
+                    score += CalculateDiagonalValueTwo(board, topLeft, player);
+                }
+                else
+                {
+                    score += CalculateColumnValueTwo(board, topLeft, player);
+                    score += CalculateRowValueThree(board, topLeft, player);
+                }
+            }
+            else
+            {
+                if (originMove.Y == 0)
+                {
+                    score += CalculateRowValueOne(board, topLeft, player);
+                    score += CalculateDiagonalValueTwo(board, topLeft, player);
+                    score += CalculateColumnValueThree(board, topLeft, player);
+                }
+                else if (originMove.Y == 1)
+                {
+                    score += CalculateRowValueTwo(board, topLeft, player);
+                    score += CalculateColumnValueThree(board, topLeft, player);
+                }
+                else
+                {
+                    score += CalculateColumnValueThree(board, topLeft, player);
+                    score += CalculateRowValueThree(board, topLeft, player);
+                    score += CalculateDiagonalValueOne(board, topLeft, player); ;
+                }
+            }
+
+            return score;
+        }
+
+        public int CheckGameWonOrFull(int rowScore, string[,] macroboard, string[,] board, Move move, Move topLeft, string p1, string p2)
+        {
+            var originMove = new Move(move.X - topLeft.X, move.Y - topLeft.Y);
+            var microboardMove = new Move(originMove.X * 3, originMove.Y * 3);
+            if (macroboard[originMove.X, originMove.Y].Equals(p1) || macroboard[originMove.X, originMove.Y].Equals(p2) || CheckEnemyCanWinTile(board, GetTopLeft(microboardMove), p1))
+            {
+                return 0;
+            }
+
+            return rowScore;
+        }
+
+        public bool CheckEnemyCanWinTile(string[,] board, Move topLeft, string player)
+        {
+            int worst = 0;
+            worst = WorstScore(worst, CalculateRowValueOne(board, topLeft, player));
+            worst = WorstScore(worst, CalculateRowValueTwo(board, topLeft, player));
+            worst = WorstScore(worst, CalculateRowValueThree(board, topLeft, player));
+            worst = WorstScore(worst, CalculateColumnValueOne(board, topLeft, player));
+            worst = WorstScore(worst, CalculateColumnValueTwo(board, topLeft, player));
+            worst = WorstScore(worst, CalculateColumnValueThree(board, topLeft, player));
+            worst = WorstScore(worst, CalculateDiagonalValueOne(board, topLeft, player));
+            worst = WorstScore(worst, CalculateDiagonalValueTwo(board, topLeft, player));
+
+            return worst <= -2;
+        }
+
+        public int WorstScore(int worst, int current)
+        {
+            return current < worst ? current : worst;
+        }
+        public int CalculateRowValueOne(string[,] board, Move topLeft, string player)
+        {
+            var score = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                if (board[topLeft.X + i, topLeft.Y].Equals(player))
+                {
+                    score += 3;
+                }
+                else if (board[topLeft.X + i, topLeft.Y].Equals(empty))
+                {
+                    score += 1;
+                }
+                else
+                {
+                    score -= 2;
+                }
+            }
+
+            return score;
+        }
+
+        public int CalculateRowValueTwo(string[,] board, Move topLeft, string player)
+        {
+            var score = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                if (board[topLeft.X + i, topLeft.Y + 1].Equals(player))
+                {
+                    score += 3;
+                }
+                else if (board[topLeft.X + i, topLeft.Y + 1].Equals(empty))
+                {
+                    score += 1;
+                }
+                else
+                {
+                    score -= 2;
+                }
+            }
+
+            return score;
+        }
+
+        public int CalculateRowValueThree(string[,] board, Move topLeft, string player)
+        {
+            var score = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                if (board[topLeft.X + i, topLeft.Y + 2].Equals(player))
+                {
+                    score += 3;
+                }
+                else if (board[topLeft.X + i, topLeft.Y + 2].Equals(empty))
+                {
+                    score += 1;
+                }
+                else
+                {
+                    score -= 2;
+                }
+            }
+
+            return score;
+        }
+
+        public int CalculateColumnValueOne(string[,] board, Move topLeft, string player)
+        {
+            var score = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                if (board[topLeft.X, topLeft.Y + i].Equals(player))
+                {
+                    score += 3;
+                }
+                else if (board[topLeft.X, topLeft.Y + i].Equals(empty))
+                {
+                    score += 1;
+                }
+                else
+                {
+                    score -= 2;
+                }
+            }
+
+            return score;
+        }
+
+        public int CalculateColumnValueTwo(string[,] board, Move topLeft, string player)
+        {
+            var score = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                if (board[topLeft.X + 1, topLeft.Y + i].Equals(player))
+                {
+                    score += 3;
+                }
+                else if (board[topLeft.X + 1, topLeft.Y + i].Equals(empty))
+                {
+                    score += 1;
+                }
+                else
+                {
+                    score -= 2;
+                }
+            }
+
+            return score;
+        }
+
+        public int CalculateColumnValueThree(string[,] board, Move topLeft, string player)
+        {
+            var score = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                if (board[topLeft.X + 2, topLeft.Y + i].Equals(player))
+                {
+                    score += 3;
+                }
+                else if (board[topLeft.X + 2, topLeft.Y + i].Equals(empty))
+                {
+                    score += 1;
+                }
+                else
+                {
+                    score -= 2;
+                }
+            }
+
+            return score;
+        }
+
+        public int CalculateDiagonalValueOne(string[,] board, Move topLeft, string player)
+        {
+            var score = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                if (board[topLeft.X + i, topLeft.Y + i].Equals(player))
+                {
+                    score += 3;
+                }
+                else if (board[topLeft.X + i, topLeft.Y + i].Equals(empty))
+                {
+                    score += 1;
+                }
+                else
+                {
+                    score -= 2;
+                }
+            }
+
+            return score;
+        }
+
+        public int CalculateDiagonalValueTwo(string[,] board, Move topLeft, string player)
+        {
+            var score = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                if (board[topLeft.X + i, topLeft.Y + 2 - i].Equals(player))
+                {
+                    score += 3;
+                }
+                else if (board[topLeft.X + i, topLeft.Y + 2 - i].Equals(empty))
+                {
+                    score += 1;
+                }
+                else
+                {
+                    score -= 2;
+                }
+            }
+
+            return score;
         }
 
     }
